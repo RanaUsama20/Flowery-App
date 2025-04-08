300f4fb71a228f63728806d0ec36aba79dc0dc82
Rana Osama <66523119+RanaUsama20@users.noreply.github.com>
2025-04-09 00:45:08 +0200

    feat(best-seller) ui and logic done

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
               theme: AppTheme.lightTheme,
               title: AppValues.appTitle,
               onGenerateRoute: RouteGenerator.getRoute,
-              initialRoute:  isLogin()?Routes.bestSeller: Routes.login,
+              initialRoute:  isLogin()?Routes.appSection: Routes.login,
              // initialRoute:  Routes.login,
 
 
