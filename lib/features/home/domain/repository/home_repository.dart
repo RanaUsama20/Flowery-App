45ff7f408bd791b92a195d811c462220f83fd296
Mj1234ba <bakriaweja@gmail.com>
2025-04-10 17:13:03 +0300

    feat(hoem_screen) finish from domain layer

--- lib/features/home/domain/repository/home_repository.dart
+++ lib/features/home/domain/repository/home_repository.dart
@@ -1,3 +1,6 @@
+import 'package:flowery_app/core/network/common/api_result.dart';
+import 'package:flowery_app/features/home/domain/entity/home_entity.dart';
 
-abstract class HomeRepository{
+abstract class HomeRepository {
+  Future<Result<HomeEntity>>getHomedata();
 }
--- lib/features/home/domain/usecase/home_use_case.dart
+++ lib/features/home/domain/usecase/home_use_case.dart
@@ -1,6 +1,11 @@
+import 'package:flowery_app/core/network/common/api_result.dart';
+import 'package:flowery_app/features/home/domain/entity/home_entity.dart';
+import 'package:flowery_app/features/home/domain/repository/home_repository.dart';
 import 'package:injectable/injectable.dart';
+
 @injectable
 class HomeUseCase {
-
-
+  final HomeRepository _homeRepository;
+  HomeUseCase(this._homeRepository);
+  Future<Result<HomeEntity>> call() => _homeRepository.getHomedata();
 }
