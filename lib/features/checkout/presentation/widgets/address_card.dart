30677624ae594cd3614e65e58590f6293750e786
Rana Osama <66523119+RanaUsama20@users.noreply.github.com>
2025-04-25 18:54:01 +0300

    fix(address card):
    fixed overflow

--- lib/features/checkout/presentation/widgets/address_card.dart
+++ lib/features/checkout/presentation/widgets/address_card.dart
@@ -53,18 +53,18 @@ class AddressCard extends StatelessWidget {
                 ],
               ),
             ),
-            Row(
-              children: [
                 Padding(
                   padding:
                   const EdgeInsets.only(left: 24, right: 32, bottom: 16),
                   child: Text(
-                    '${address.street} ${address.city}',
+                    '${address.street} ',
                     style: Theme.of(context).textTheme.bodySmall,
+                      maxLines:1,
+                      overflow: TextOverflow.ellipsis,
                   ),
                 ),
-              ],
-            ),
+
+
           ],
         ),
       ),
