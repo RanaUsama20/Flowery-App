c22f4905851bed7ea976aea81d9e9abc0e1a1353
Amira <150848650+Amira098@users.noreply.github.com>
2025-04-25 16:55:25 +0300

    feat( filter): make test

--- lib/features/categories/data/repository_imp/getCategories_repository_impl.dart
+++ lib/features/categories/data/repository_imp/getCategories_repository_impl.dart
@@ -1,6 +1,5 @@
 import 'package:flowery_app/features/categories/domain/entity/get_products_by_id_entity.dart';
 import 'package:injectable/injectable.dart';
-
 import '../../domain/entity/get_all_categories_entity.dart';
 import '../../domain/repository/getCategories_repository.dart';
 import '../data_source/getCategories_data_source.dart';
@@ -11,22 +10,18 @@ class GetCategoriesRepositoryImpl implements GetCategoriesRepository {
   GetAllCategoriesDataSource getCategoriesDataSource;
   GetCategoriesRepositoryImpl(this.getCategoriesDataSource,);
 
+  @override
   Future<GetAllCategoriesEntity> getAllCategories() async {
     var response=await getCategoriesDataSource.getAllCategories();
     return response;
   }
 
-}
-
-@Injectable(as: GetProductsByIdRepository)
-class GetProductsByIdRepositoryImpl implements GetProductsByIdRepository {
-  final GetProductsByIdDataSource getProductsDataSource;
-
-  GetProductsByIdRepositoryImpl(this.getProductsDataSource);
 
   @override
   Future<ProductsModelEntity> getProductsById(String categoryId) async {
-    return await getProductsDataSource.getProductsById(categoryId);
+    return await getCategoriesDataSource.getProductsById(categoryId);
   }
+
 }
+
 
