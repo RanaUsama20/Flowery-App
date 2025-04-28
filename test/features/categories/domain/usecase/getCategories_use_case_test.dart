e847b0bd1822f7b62c084ac67be893ebd965292c
Amira <150848650+Amira098@users.noreply.github.com>
2025-04-28 20:24:01 +0300

    build(Integrate Firebase):Integrate Firebase Crashlytics,Receive Notification

---
+++ test/features/categories/domain/usecase/getCategories_use_case_test.dart
@@ -0,0 +1,49 @@
+import 'package:flowery_app/features/categories/domain/entity/get_all_categories_entity.dart';
+import 'package:flowery_app/features/categories/domain/entity/get_products_by_id_entity.dart';
+import 'package:flowery_app/features/categories/domain/repository/getCategories_repository.dart';
+import 'package:flowery_app/features/categories/domain/usecase/getCategories_use_case.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'getCategories_use_case_test.mocks.dart';
+
+@GenerateMocks([GetCategoriesRepository])
+
+void main() {
+  late MockGetCategoriesRepository mockRepository;
+  late GetCategoriesUseCase useCase;
+
+  setUp(() {
+    mockRepository = MockGetCategoriesRepository();
+    useCase = GetCategoriesUseCase(mockRepository);
+  });
+
+  test('should return list of CategoriesEntity from repository', () async {
+
+    final categories = [CategoriesEntity(id: '1', name: 'Test', slug: '', image: '', createdAt: '', updatedAt: '', productsCount: 0)];
+    final response = GetAllCategoriesEntity(categories: categories);
+    when(mockRepository.getAllCategories()).thenAnswer((_) async => response);
+
+
+    final result = await useCase.getAllCategories();
+
+    expect(result, equals(categories));
+    verify(mockRepository.getAllCategories()).called(1);
+  });
+
+  test('should return list of ProductsEntity from repository by categoryId', () async {
+
+    const categoryId = '1';
+    final products = [ProductsEntity(id: '101', )];
+    final response = ProductsModelEntity(products: products);
+    when(mockRepository.getProductsById(categoryId)).thenAnswer((_) async => response);
+
+
+    final result = await useCase.getProductsById(categoryId);
+
+
+    expect(result, equals(products));
+    verify(mockRepository.getProductsById(categoryId)).called(1);
+  });
+}
