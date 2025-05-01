197e5b253dec17a7404bcbccfa9a3acb573fef6a
Rana Osama <66523119+RanaUsama20@users.noreply.github.com>
2025-05-01 18:50:34 +0300

    merged saved_address

--- lib/features/profile/presentation/view/saved_adreess.dart
+++ lib/features/profile/presentation/view/saved_adreess.dart
@@ -24,7 +24,7 @@ class _SavedAddressState extends State<SavedAddress> {
     super.initState();
 
     cubit = context.read<ProfileMainCubit>();
-    // cubit.getProfileData()
+     cubit.getProfileData();
   }
 
   Future<void> deleteAddress(String id) async {
