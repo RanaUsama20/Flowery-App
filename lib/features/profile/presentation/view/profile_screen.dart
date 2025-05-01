a5e299b0e39efd52abb212ea3e8941688bd53018
Rana Osama <66523119+RanaUsama20@users.noreply.github.com>
2025-05-01 22:50:47 +0300

    hotfix

--- lib/features/profile/presentation/view/profile_screen.dart
+++ lib/features/profile/presentation/view/profile_screen.dart
@@ -16,6 +16,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
+import '../../../../core/app/app_cubit/app_cubit_cubit.dart';
+import '../../../../core/enum/state_user.dart';
 import '../../../../core/utils/widgets/error_widget.dart';
 import '../../../auth/data/model/request/edit_profile_request.dart';
 import '../view_model/profile_main/profile_main_cubit.dart';
@@ -27,20 +29,25 @@ class ProfileScreen extends StatefulWidget {
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
-
 class _ProfileScreenState extends State<ProfileScreen> {
   late ProfileMainCubit cubit;
+  late AppCubit _appCubit;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     cubit = serviceLocator<ProfileMainCubit>();
-    cubit.getProfileData();
+    _appCubit = serviceLocator<AppCubit>();
+    if (_appCubit.getStateUser == StateUser.login) {
+      cubit.getProfileData();
+    }
   }
+
   bool isOn = true;
   @override
   Widget build(BuildContext context) {
-   return  BlocBuilder<ProfileMainCubit, ProfileMainState>(
+    return BlocBuilder<ProfileMainCubit, ProfileMainState>(
       builder: (context, state) {
         if (state.isProfileMainLoading) {
           return Skeletonizer(
@@ -61,151 +68,168 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     context.read<ProfileMainCubit>().getProfileData()),
           );
         }
+        if (state.isProfileMainSuccess){
+        return Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          _customAppBar(),
+          const SizedBox(height: 16),
+          Expanded(
+            child: Column(
+              children: [
+                _topSectionDetails(
+                  email: state.profileData.user.email,
+                  editProfileData: EditProfileRequest(
+                      firstName: state.profileData.user.firstName,
+                      lastName: state.profileData.user.lastName,
+                      email: state.profileData.user.email,
+                      phone: state.profileData.user.phone,
+                      url: state.profileData.user.photo),
+                  name:
+                      "${state.profileData.user.firstName} ${state.profileData.user.lastName}",
+                  imageUrl: state.profileData.user.photo,
+                ),
+                const SizedBox(height: 32),
+                Expanded(
+                    child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      _itemSection(
+                        leading: _iconSvg(SvgAssets.order),
+                        trailing: _arrowIos(),
+                        title: LocaleKeys.profile_MyOrder.tr(),
+                        onTap: () {
+                          Navigator.of(context).pushNamed(Routes.order);
+                        },
+                      ),
+                      _itemSection(
+                        leading: _iconSvg(SvgAssets.location2Svg),
+                        trailing: _arrowIos(),
+                        title: LocaleKeys.profile_SavedAddress.tr(),
+                        onTap: () {
+                          final state = context.read<ProfileMainCubit>().state;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        _customAppBar(),
-        const SizedBox(height: 16),
-        Expanded(
+                          if (state.isProfileMainSuccess) {
+                            print("------------------------------");
+                            print(
+                              state.profileData.user.addresses,
+                            );
+                            Navigator.pushNamed(
+                              context,
+                              Routes.savedAddress,
+                              arguments: state.profileData.user.addresses,
+                            ).then((result) {
+                              if (result == 'refresh') {
+                                context
+                                    .read<ProfileMainCubit>()
+                                    .getProfileData();
+                              }
+                            });
+                          }
+                        },
+                      ),
+                      SizedBox(height: 16),
+                      const Divider(),
+                      SizedBox(height: 16),
+                      _itemSection(
+                        leading: CustomSwitch(
+                          width: 42,
+                          height: 20,
+                          value: isOn,
+                          onChanged: (value) {
+                            setState(() {
+                              isOn = value;
+                            });
+                          },
+                        ),
+                        trailing: _arrowIos(),
+                        title: LocaleKeys.profile_Notification.tr(),
+                        onTap: () {},
+                      ),
+                      SizedBox(height: 16),
+                      const Divider(),
+                      SizedBox(height: 16),
+                      _itemSection(
+                        leading: _iconSvg(SvgAssets.translate),
+                        trailing: Text(
+                          context.locale.languageCode == AppValues.english
+                              ? LocaleKeys.profile_English.tr()
+                              : LocaleKeys.profile_Arabic.tr(),
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                        title: LocaleKeys.profile_Language.tr(),
+                        onTap: () {
+                          _showBottomSheetLang();
+                        },
+                      ),
+                      _itemSection(
+                        trailing: _arrowIos(),
+                        title: LocaleKeys.profile_AboutUs.tr(),
+                        onTap: () {
+                          Navigator.of(context).pushNamed(Routes.aboutApp);
+                        },
+                      ),
+                      _itemSection(
+                        trailing: _arrowIos(),
+                        title: LocaleKeys.profile_TermsConditions.tr(),
+                        onTap: () {
+                          Navigator.of(context)
+                              .pushNamed(Routes.termsAndCondition);
+                        },
+                      ),
+                      SizedBox(height: 16),
+                      const Divider(),
+                      SizedBox(height: 16),
+                      BlocListener<ProfileMainCubit, ProfileMainState>(
+                        listener: (context, state) {
+                          if (state.isLogoutLoading) {
+                            AppDialogs.showLoadingDialog(context);
+                          } else if (state.isLogoutSuccess) {
+                            Navigator.pushNamed(context, Routes.appSection);
+                          } else if (state.isLogoutFailure) {
+                            AppDialogs.showFailureDialog(context,
+                                message: state.logOutMessageResponse);
+                          }
+                        },
+                        child: _itemSection(
+                          trailing: Icon(Icons.login_outlined),
+                          title: LocaleKeys.profile_Logout.tr(),
+                          onTap: () {
+                            context.read<ProfileMainCubit>().logout();
+                          },
+                        ),
+                      ),
+                      const SizedBox(),
+                      Text(
+                        "v 6.3.0 - (446)",
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              color: AppColors.white[AppColors.colorCode90],
+                            ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ))
+              ],
+            ),
+          )
+        ]);}
+       return Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              _topSectionDetails(
-                email: state.profileData.user.email,
-                editProfileData: EditProfileRequest(
-                    firstName: state.profileData.user.firstName,
-                    lastName: state.profileData.user.lastName,
-                    email: state.profileData.user.email,
-                    phone: state.profileData.user.phone,
-                    url: state.profileData.user.photo),
-                name:
-                "${state.profileData.user.firstName} ${state.profileData.user.lastName}",
-                imageUrl: state.profileData.user.photo,
-              ),
-              const SizedBox(height: 32),
-              Expanded(
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        _itemSection(
-                          leading: _iconSvg(SvgAssets.order),
-                          trailing: _arrowIos(),
-                          title: LocaleKeys.profile_MyOrder.tr(),
-                          onTap: () {
-                            Navigator.of(context).pushNamed(Routes.order);
-                          },
-                        ),
-                        _itemSection(
-                          leading: _iconSvg(SvgAssets.location2Svg),
-                          trailing: _arrowIos(),
-                          title: LocaleKeys.profile_SavedAddress.tr(),
-                          onTap: () {
-                            final state = context.read<ProfileMainCubit>().state;
-
-                            if (state.isProfileMainSuccess) {
-                              print("------------------------------");
-                              print(
-                                state.profileData.user.addresses,
-                              );
-                              Navigator.pushNamed(
-                                context,
-                                Routes.savedAddress,
-                                arguments: state.profileData.user.addresses,
-                              ).then((result) {
-                                if (result == 'refresh') {
-                                  context
-                                      .read<ProfileMainCubit>()
-                                      .getProfileData();
-                                }
-                              });
-                            }
-                          },
-                        ),
-                        SizedBox(height: 16),
-                        const Divider(),
-                        SizedBox(height: 16),
-                        _itemSection(
-                          leading: CustomSwitch(
-                            width: 42,
-                            height: 20,
-                            value: isOn,
-                            onChanged: (value) {
-                              setState(() {
-                                isOn = value;
-                              });
-                            },
-                          ),
-                          trailing: _arrowIos(),
-                          title: LocaleKeys.profile_Notification.tr(),
-                          onTap: () {},
-                        ),
-                        SizedBox(height: 16),
-                        const Divider(),
-                        SizedBox(height: 16),
-                        _itemSection(
-                          leading: _iconSvg(SvgAssets.translate),
-                          trailing: Text(
-                            context.locale.languageCode == AppValues.english
-                                ? LocaleKeys.profile_English.tr()
-                                : LocaleKeys.profile_Arabic.tr(),
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          ),
-                          title: LocaleKeys.profile_Language.tr(),
-                          onTap: () {
-                            _showBottomSheetLang();
-                          },
-                        ),
-                        _itemSection(
-                          trailing: _arrowIos(),
-                          title: LocaleKeys.profile_AboutUs.tr(),
-                          onTap: () {
-                            Navigator.of(context).pushNamed(Routes.aboutApp);
-                          },
-                        ),
-                        _itemSection(
-                          trailing: _arrowIos(),
-                          title: LocaleKeys.profile_TermsConditions.tr(),
-                          onTap: () {
-                            Navigator.of(context)
-                                .pushNamed(Routes.termsAndCondition);
-                          },
-                        ),
-                        SizedBox(height: 16),
-                        const Divider(),
-                        SizedBox(height: 16),
-                        BlocListener<ProfileMainCubit, ProfileMainState>(
-                          listener: (context, state) {
-                            if (state.isLogoutLoading) {
-                              AppDialogs.showLoadingDialog(context);
-                            } else if (state.isLogoutSuccess) {
-                              Navigator.pushNamed(context, Routes.appSection);
-                            } else if (state.isLogoutFailure) {
-                              AppDialogs.showFailureDialog(context,
-                                  message: state.logOutMessageResponse);
-                            }
-                          },
-                          child: _itemSection(
-                            trailing: Icon(Icons.login_outlined),
-                            title: LocaleKeys.profile_Logout.tr(),
-                            onTap: () {
-                              context.read<ProfileMainCubit>().logout();
-                            },
-                          ),
-                        ),
-                        const SizedBox(),
-                        Text(
-                          "v 6.3.0 - (446)",
-                          style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                            color: AppColors.white[AppColors.colorCode90],
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
-                    ),
-                  ))
+              Text(LocaleKeys.Error_YouHaveToLoginToUseThisFeature.tr()),
+              SizedBox(height: 20),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding:
+                    EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(Routes.login);
+                  },
+                  child: Text(LocaleKeys.Authentication_Login.tr()))
             ],
           ),
-        )]);
+        );
 
       },
     );
