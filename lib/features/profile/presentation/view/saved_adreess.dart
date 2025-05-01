fdc092de59a687fa9e1071a59b20f257977b8b70
Rana Osama <66523119+RanaUsama20@users.noreply.github.com>
2025-05-01 20:25:55 +0300

    lottie

--- lib/features/profile/presentation/view/saved_adreess.dart
+++ lib/features/profile/presentation/view/saved_adreess.dart
@@ -62,7 +62,11 @@ class _SavedAddressState extends State<SavedAddress> {
       },
       builder: (context, state) {
         final addresses = state.profileData.user.addresses;
+        if(state.isProfileMainLoading)
+        {
+          return  Scaffold(body: Center(child: CircularProgressIndicator()),);
 
+        }
         return Scaffold(
           body: SafeArea(
             child: Padding(
